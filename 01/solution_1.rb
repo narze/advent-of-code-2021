83094d9478387f14fc523b5c741c623530b1b693
Manassarn Manoonchai <manassarn@gmail.com>
2021-12-01 22:03:02 +0700

    Refactor

--- 01/solution_1.rb
+++ 01/solution_1.rb
@@ -1,11 +1,12 @@
 count = -1
 prev_line = 0
-File.read(ARGV[0]).split("\n").each do |line|
-  if line.to_i > prev_line
+
+File.read(ARGV[0]).split("\n").map(&:to_i).each do |line|
+  if line > prev_line
     count += 1
   end
 
-  prev_line = line.to_i
+  prev_line = line
 end
 
 puts "Output : #{count}"
--- 01/solution_2.rb
+++ 01/solution_2.rb
@@ -1,10 +1,8 @@
 count = -1
 prev_input = 0
 
-inputs = File.read(ARGV[0]).split("\n").map(&:to_i)
-
-(0...inputs.size-2).each do |idx|
-  input = inputs[idx] + inputs[idx+1] + inputs[idx+2]
+File.read(ARGV[0]).split("\n").map(&:to_i).each_cons(3) do |arr|
+  input = arr.sum
 
   if input > prev_input
     count += 1
