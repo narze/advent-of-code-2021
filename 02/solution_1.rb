8f7539a33c833dfc5b714bbdfc63b95311ab01ef
Manassarn Manoonchai <manassarn@gmail.com>
2021-12-02 23:28:09 +0700

    Day 2

---
+++ 02/solution_1.rb
@@ -0,0 +1,20 @@
+x = 0
+y = 0
+
+File.read(ARGV[0]).split("\n").each do |line|
+  pos, unit = line.split(" ")
+  unit = unit.to_i
+
+  case pos
+  when "forward"
+    x += unit
+  when "up"
+    y -= unit
+  when "down"
+    y += unit
+  else
+    raise "Bad input"
+  end
+end
+
+puts "Output : #{x * y}"
---
+++ 02/solution_2.rb
@@ -0,0 +1,22 @@
+x = 0
+y = 0
+a = 0
+
+File.read(ARGV[0]).split("\n").each do |line|
+  pos, unit = line.split(" ")
+  unit = unit.to_i
+
+  case pos
+  when "forward"
+    x += unit
+    y += a * unit
+  when "up"
+    a -= unit
+  when "down"
+    a += unit
+  else
+    raise "Bad input"
+  end
+end
+
+puts "Output : #{x * y}"
